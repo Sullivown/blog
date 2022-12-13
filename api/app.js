1f902067e20cd04effd8f5611b753421642d33e1
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-12-13 12:06:27 +0000

    set up mongoose connection

--- api/app.js
+++ api/app.js
@@ -5,11 +5,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set up mongoose connection
+const mongoDB = process.env.DB_CONNECT;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
